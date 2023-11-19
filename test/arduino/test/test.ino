b495716962a8ae370d72a97974b6e9162f8ca23e
LoganHaug <logannhaug@gmail.com>
2023-11-19 15:32:29 -0800

    Add test udp arduino code

---
+++ test/arduino/test/test.ino
@@ -0,0 +1,38 @@
+#include <SPI.h>         // needed for Arduino versions later than 0018
+#include <Ethernet.h>
+#include <EthernetUdp.h>         // UDP library from: bjoern@cs.stanford.edu 12/30/2008
+
+
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = {  
+  0xA8, 0x61, 0x0A, 0xAE, 0x95, 0xE3 };
+IPAddress ip(192, 168, 1, 177);
+
+unsigned int localPort = 8888;      // local port to listen on
+
+// An EthernetUDP instance to let us send and receive packets over UDP
+EthernetUDP Udp;
+
+void setup() {
+  // start the Ethernet and UDP:
+  Ethernet.begin(mac,ip);
+  Udp.begin(localPort);
+
+  Serial.begin(9600);
+  Serial.println("Starting connection");
+}
+
+void loop() {
+  
+  // if there's data available, read a packet
+  int packetSize = Udp.parsePacket();
+  if(packetSize)
+  {
+    Serial.print("Received packet of size ");
+    Serial.println(packetSize);
+  }
+  delay(10);
+  
+}
+

58b918b8004513834937fe69c63e48bfb259b4fa
JustinVFong <justinvf2018@gmail.com>
2023-11-19 14:58:14 -0800

    possible UDP

--- top_side/clientTCP.py
+++ top_side/clientTCP.py
@@ -1,29 +1,19 @@
 import socket
 
-HEADER = 64
-PORT = 5050
+PORT = 5051
 FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "!DISCONNECT"
 SERVER = socket.gethostbyname(socket.gethostname())
-SERVER = "192.168.1.177"
+# SERVER = "192.168.1.177"
 ADDR = (SERVER, PORT)
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
-
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 
 def send(msg):
     message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
+    client.sendto(message, ADDR)
 
-    # need to subtract 64 by str(msg_length).encode(FORMAT)
-    # that will figure how much to pad the message so it is the length 64
-
-    send_length += b' ' * (HEADER-len(send_length)) #b' ' adds blank spaces
-    client.send(send_length)
-    client.send(message)
 
 send("Hello World")
 input()

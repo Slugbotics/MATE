e201bd4e91fd7c850d946fa208c1f0e0c3a9a2f8
Ben Hess <pilotben@comcast.net>
2023-12-03 14:30:29 -0800

    Input testing

--- top_side/client.py
+++ top_side/client.py
@@ -1,5 +1,12 @@
 # importing the module
 import socket
+import input
+import time
+
 # creating a socket and connection between the devices --> sock-stream is a connection oriented TCP
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client.sendto(b"Hello!", ("192.168.1.177", 8888))
+
+while True:
+    packet = str(input.left_stick) + " " + str(input.right_stick) + " " + str(input.x_pressed)
+    client.sendto(packet.encode(), ("192.168.1.177", 8888))
+    time.sleep(0.1)

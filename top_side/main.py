4c24e771c4da642f3f4ad88c21446c88079f1226
Ben Hess <pilotben@comcast.net>
2024-01-21 14:41:07 -0800

    Merged branch 'input'

--- top_side/main.py
+++ top_side/main.py
@@ -1,15 +1,34 @@
 import input
 import socket
 import time
+import math
 
-# creating a socket and connection between the devices --> sock-stream is a connection oriented TCP
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 client.bind(("192.168.1.155", 8888))
 
 while True:
-    packet = str(input.left_stick) + ", " + str(input.right_stick) + ", " + str(input.x_pressed)
+    top_l = (1/math.sqrt(2), 1/math.sqrt(2))
+    top_r = (-1/math.sqrt(2), 1/math.sqrt(2))
+    bot_l = (-1/math.sqrt(2), 1/math.sqrt(2))
+    bot_r = (1/math.sqrt(2), 1/math.sqrt(2))
+
+    input_x, input_y = input.left_stick
+
+    magnitude = math.sqrt(input_x ** 2 + input_y ** 2)
+
+    if magnitude > 1:
+        input_x = input_x / magnitude
+        input_y = input_y / magnitude
+
+    top_left = top_l[0] * input_x + top_l[1] * input_y
+    top_right = top_r[0] * input_x + top_r[1] * input_y
+    bot_left = bot_l[0] * input_x + bot_l[1] * input_y
+    bot_right = bot_r[0] * input_x + bot_r[1] * input_y
+    
+    packet = str(top_left) + ", " + str(top_right) + ", " + str(bot_left) + ", " + str(bot_right)
     client.sendto(packet.encode(), ("192.168.1.177", 8888))
     message, addr = client.recvfrom(2000)
     print(message)
+
     time.sleep(0.1)

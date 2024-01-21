4a8065643a892d5a2bbf6fdea6082fc3b2625233
Ben Hess <pilotben@comcast.net>
2024-01-21 13:29:18 -0800

    Basic communication test.

--- top_side/client.py
+++ top_side/client.py
@@ -1,6 +1,5 @@
 # importing the module
 import socket
-import input
 import time
 
 # creating a socket and connection between the devices --> sock-stream is a connection oriented TCP
@@ -9,7 +8,7 @@ client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client.bind(("192.168.1.155", 8888))
 
 while True:
-    packet = str(input.left_stick) + " " + str(input.right_stick) + " " + str(input.x_pressed)
+    packet = "hello"
     client.sendto(packet.encode(), ("192.168.1.177", 8888))
     message, addr = client.recvfrom(2000)
     print(message)

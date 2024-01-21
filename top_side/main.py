24fc62785dd36d8d531139df64a8fecad83534ba
Ben Hess <pilotben@comcast.net>
2024-01-21 14:02:22 -0800

    Integrated input test with communication.

--- top_side/main.py
+++ top_side/main.py
@@ -1,6 +1,15 @@
 import input
+import socket
 import time
 
+# creating a socket and connection between the devices --> sock-stream is a connection oriented TCP
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+client.bind(("192.168.1.155", 8888))
+
 while True:
-    print(str(input.left_stick) + " " + str(input.right_stick) + " " + str(input.x_pressed))
+    packet = str(input.left_stick) + ", " + str(input.right_stick) + ", " + str(input.x_pressed)
+    client.sendto(packet.encode(), ("192.168.1.177", 8888))
+    message, addr = client.recvfrom(2000)
+    print(message)
     time.sleep(0.1)

af53317543da09630e34c7239c38494401f2f4f1
LoganHaug <logannhaug@gmail.com>
2024-01-28 14:22:15 -0800

    Delete root tflite files, update requirements

--- top_side/object_detect.py
+++ top_side/object_detect.py
@@ -6,18 +6,13 @@ import detection
 obj = False
 hand = False
 
-window = tk.Tk()
 vid = cv.VideoCapture(0)
 
-window.rowconfigure(0, minsize=100)
-window.columnconfigure([0,1], minsize=200)
 
 if not vid.isOpened():
     print("Camera cant be opened")
     exit()
 
-buttons = tk.Frame(master=window)
-buttons.grid(row=0, column=0)
 
 def object_method():
     global obj
@@ -26,12 +21,7 @@ def hand_method():
     global hand
     hand = not hand
 
-button1 = tk.Button(master=buttons, text="Object Detection", command=object_method)
-button1.pack(padx=20, pady=5)
 
-button2 = tk.Button(master=buttons, text="Hand Detection", command=hand_method)
-button2.pack(padx=20, pady=5)
-# buttons.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
 def get_cam():
     _, frame = vid.read()
 
@@ -43,21 +33,11 @@ def get_cam():
     if hand:
         detection.write_marks_hands(opencv_img, copy)
     
-    img = ImageTk.PhotoImage(image=Image.fromarray(copy))
+
 
     return img
 
-img_container = tk.Label(image=get_cam())
-img_container.grid(row=0, column=1)
-
-def set_img():
-    img = get_cam()
-    img_container.photo_image = img
-    img_container.configure(image=img)
-    img_container.after(20, set_img)
 
 
-set_img()
-window.mainloop()
 vid.release()
 cv.destroyAllWindows()

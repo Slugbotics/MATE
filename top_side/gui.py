b7ee73be13c2cfc01e63fd4c05822d5d35585128
LoganHaug <logannhaug@gmail.com>
2024-01-28 14:38:18 -0800

    Add basic camera gui

--- top_side/gui.py
+++ top_side/gui.py
@@ -9,4 +9,56 @@ This program will ask for permission to read the webcam (popup)'''
 
 import PySimpleGUI as sg
 
-sg.theme
+import cv2
+
+import numpy as np
+
+
+def main():
+
+    sg.theme("LightGreen")
+
+
+    # Define the window layout
+
+    layout = [
+
+        [sg.Text("Slugbotics Driver Station", size=(60, 1), justification="center")],
+
+        [sg.Image(filename="", key="-IMAGE-")],
+
+        [sg.Button("Exit", size=(10, 1))],
+
+    ]
+
+
+    # Create the window and show it without the plot
+
+    window = sg.Window("OpenCV Integration", layout, location=(800, 400))
+
+
+    cap = cv2.VideoCapture(0)
+
+
+    while True:
+
+        event, values = window.read(timeout=20)
+
+        if event == "Exit" or event == sg.WIN_CLOSED:
+
+            break
+
+
+        ret, frame = cap.read()
+
+
+        
+        imgbytes = cv2.imencode(".png", frame)[1].tobytes()
+
+        window["-IMAGE-"].update(data=imgbytes)
+
+
+    window.close()
+
+
+main()

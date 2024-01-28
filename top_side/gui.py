1558b77c0a6aef803632380653fab6ec90c2013d
LoganHaug <logannhaug@gmail.com>
2024-01-28 14:20:50 -0800

    Move some tf files around, remove tkinter from object_detect

---
+++ top_side/gui.py
@@ -0,0 +1,12 @@
+''' Runs the driver station GUI
+
+displays:
+- camera / opencv output
+- network info
+- last input sent
+
+This program will ask for permission to read the webcam (popup)'''
+
+import PySimpleGUI as sg
+
+sg.theme

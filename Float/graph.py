4c1dbc2ae00d800a63f86d16c05403866be02c8a
LoganHaug <logannhaug@gmail.com>
2024-04-08 17:54:25 -0700

    Update the float grapher to use datetime objects and convert pressure

--- Float/graph.py
+++ Float/graph.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pygal
 
 
@@ -8,18 +10,24 @@ def graph_depth(data, time):
     depth_graph = pygal.Line()
     depth_graph.title = "Depth over time" 
     depth_graph.x_labels = time
-    depth_graph.x_title = "UTC time"
+    depth_graph.x_title = "Time"
     depth_graph.y_title = "Depth (meters)"
     graph_pressure(pressure_data, time)
     depth_graph.add("Depth", data)
     depth_graph.render_to_file("depth_graph.svg")
 
+def convert_pressure(pressure):
+    # TODO implement pressure to depth conversion
+    return pressure
 
 
 if __name__ == "__main__":
     with open("float_data.txt") as file:
         data = file.read().split("\n")
-    depth_data = [float(x) for x in data[0].split(" ")]
-    time = [float(x) for x in data[1].split(" ")]
-    graph_depth(depth_data, time)
+    time_axis = []
+    depth_axis = []
+    for time, pressure in data:
+        time_axis.append(datetime.fromisoformat(time)) 
+        depth_axis.append(convert_pressure(pressure))
+    graph_depth(depth_axis, time_axis)
 

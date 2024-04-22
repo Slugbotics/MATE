768934dc724feb57d64030c30c5ab5615d421eb1
LoganHaug <logannhaug@gmail.com>
2024-04-21 17:15:02 -0700

    Add a nonworking test of the 6 camera setup

--- top_side/gui.py
+++ top_side/gui.py
@@ -25,7 +25,8 @@ def main():
     # Define the window layout
 
     layout = [
-        [sg.Push(), sg.Image(filename="", key="-IMAGE-"), sg.Push()],
+        [sg.Image(filename="", key="-IMAGE-"), sg.Image(filename="", key="-IMAGE-"), sg.Image(filename="", key="-IMAGE-")],
+        [sg.Image(filename="", key="-IMAGE-"), sg.Image(filename="", key="-IMAGE-"), sg.Image(filename="", key="-IMAGE-")],
         [sg.StatusBar("Network Status"), sg.StatusBar("Last Input Sent")],
         [sg.StatusBar("Thruster 1 status"), sg.StatusBar("Thruster 2 status")],
         [sg.StatusBar("Thruster 3 status"), sg.StatusBar("Thruster 4 status")],
@@ -36,37 +37,34 @@ def main():
 
 
     window = sg.Window("Slugbotics Driver Station", layout, location=(0, 0), resizable=True)
+    window.read(timeout=20)
+    window.maximize()
 
-    cap = cv2.VideoCapture(0)
-    if not cap.isOpened():
-        print("Camera cannot be accessed")
-        exit()
-
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-    cap.set(cv2.CAP_PROP_FPS, 60)
-    window_maximized = False
+    cams = []
+    for i in range(0,6):
+        cams.append(cv2.VideoCapture(i))
+        if not cams[i].isOpened():
+            print(f"Camera: {i} failed to capture, skipping")
+            continue
+        cams[i].set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        cams[i].set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
+        cams[i].set(cv2.CAP_PROP_FPS, 60)
     # Event loop
     while True:
-
         event, values = window.read(timeout=20)
-
-        if not window_maximized:
-            window.maximize()
-        
         if event == "Exit" or event == sg.WIN_CLOSED:
             break
 
-
-        ret, frame = cap.read()
-
-        frame = cv2.resize(frame, (round(1920 * 0.85), round(1080 * 0.85)))
-        imgbytes = cv2.imencode(".png", frame)[1].tobytes()
-        window["-IMAGE-"].update(data=imgbytes)
+        for cam in cams:
+            ret, frame = cam.read()
+            frame = cv2.resize(frame, (640, 360))
+            imgbytes = cv2.imencode(".png", frame)[1].tobytes()
+            window["-IMAGE-"].update(data=imgbytes)
 
 
     window.close()
-    cv2.VideoCapture(2).release()
+    for i in range(len(cams)): 
+        cv2.VideoCapture(i).release()
 
 
 if __name__ == "__main__":

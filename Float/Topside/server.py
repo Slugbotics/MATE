c7281aebb95b781fc1b138a5db9a3bf54ba1b24c
Ruthwika Gajjala <rgajjala@ucsc.edu>
2024-05-16 21:34:50 -0700

    fixed server

--- Float/Topside/server.py
+++ Float/Topside/server.py
@@ -34,8 +34,8 @@ def send_file():
     print(f"accepted connection from {client_address[0]}:{client_address[1]}")
 
     filename = "float_data.txt"
-    client_socket.send(filename.encode("utf-8"))
-    print(f"Sent: {filename}")
+    # client_socket.send(filename.encode("utf-8"))
+    # print(f"Sent: {filename}")
     file = open(filename, "r")
     data = file.read()
     client_socket.send(data.encode("utf-8"))
@@ -44,24 +44,5 @@ def send_file():
     client_socket.close()
     print("connection to client closed")
     server.close()
-
-def receive_file():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_ip = "100.64.60.135"
-    server_port = 8000
-    client.connect((server_ip, server_port))
-
-    filename = client.recv(1024).decode("utf-8")
-    print(f"Received: {filename}")
-    file = open(filename, "w")
-
-    data = client.recv(1024).decode("utf-8")
-    print(f"[RECV] Receiving the file data.")
-    file.write(data)
-    client.send("File data received".encode("utf-8"))
-
-    file.close()
-    client.close()
-    print("connection to server closed")
 
 send_file()

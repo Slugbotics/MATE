95aef927d0c31765a173417b61db1a309a2803fc
Logan Haug <logannhaug@gmail.com>
2024-05-21 17:47:53 -0700

    Update videocapture backend to use vidgear

--- top_side/camera_gui.py
+++ top_side/camera_gui.py
@@ -4,29 +4,28 @@ displays one camera in a window
 submodule for driver station gui'''
 
 import cv2
+from vidgear.gears import CamGear
 import PySimpleGUI as sg
 
 import time
 
-window_width = 640
-window_height = 360
+cam_width = 1280 
+cam_height = 720 
 # screen_width = 1920
 # screen_height = 1080
+options = {"CAP_PROP_FPS": 20, 
+           "CAP_PROP_FRAME_WIDTH": cam_width, 
+           "CAP_PROP_FRAME_HEIGHT": cam_height}
 
 def display_camera(device_num: int, window) -> None:
     """Thread function: takes in a camera file location, displays video to ui""" 
     
-    cam = cv2.VideoCapture(device_num)
-    if not cam.isOpened():
-        print(f"Error: camera {device_num} could not be loaded")
-        return -1
-
-    cam.set(cv2.CAP_PROP_FRAME_WIDTH, window_width)
-    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, window_height)
-    cam.set(cv2.CAP_PROP_FPS, 60)
+    cam = CamGear(source=device_num, **options).start()
 
     while True:
-        ret, frame = cam.read()
+        frame = cam.read()
+        if frame is None:
+            break
         imgbytes = cv2.imencode(".png", frame)[1].tobytes()
-        window[f"cam{device_num}"].update(data=imgbytes)
-    cam.release()
+        window[f"cam"].update(data=imgbytes) 
+    cam.stop()

1fc3402eed577bf9dc604ee4c7271cec4268a42f
Logan Haug <logannhaug@gmail.com>
2024-05-21 17:47:53 -0700

    Update videocapture backend to use vidgear

--- top_side/gui.py
+++ top_side/gui.py
@@ -3,27 +3,26 @@
 displays:
 - camera / opencv output
 - network info
-- last input sent
-- thruster data
 
 This program will ask for permission to read the webcam (popup)'''
 
 import PySimpleGUI as sg
-import cv2
-import numpy as np
-
-import glob
-import time
-import multiprocessing as mp
 
 import camera_gui
 
 def main() -> None:
+    cams = []
+    # Place each camera index on each new line in top_side/cams.txt
+    # order of cams, front, left, right, back
+    with open("cams.txt", "r") as cam_file:
+        cams = [int(x) for x in cam_file]
+    curr_device_num = cams[0]
 
     sg.theme("DarkBlack")
 
     layout = [
-        [sg.Push(), sg.Image(filename="", key="cam0"), sg.Image(filename="", key="cam2")],
+        [sg.Push(), sg.Text("Current Camera: "), sg.Text("", key="-CAM_NUM-"), sg.Push()],
+        [sg.Push(), sg.Image(filename="", key="cam"), sg.Push()],
         [sg.StatusBar("Network Status"), sg.StatusBar("Last Input Sent")],
         [sg.StatusBar("Thruster 1 status"), sg.StatusBar("Thruster 2 status")],
         [sg.StatusBar("Thruster 3 status"), sg.StatusBar("Thruster 4 status")],
@@ -31,12 +30,8 @@ def main() -> None:
 
     ]
 
-
     window = sg.Window("Slugbotics Driver Station", layout, location=(0, 0), size=(1920, 1080))
-
-    for i in [0, 2]:
-        window.start_thread(lambda: camera_gui.display_camera(i, window), ("", ""))
-
+    window.start_thread(lambda: camera_gui.display_camera(curr_device_num, window), ("", ""))
 
     # Event loop
     while True:
@@ -45,7 +40,6 @@ def main() -> None:
             break
 
     window.close()
-
  
 if __name__ == "__main__":
     main()
